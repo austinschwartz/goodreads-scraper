d4e57ce32190dd6ab982b277d260a21ae0223aab
nonis3 <nonis@purdue.edu>
2017-04-26 20:00:00 -0600

    Files

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem 'goodreads', '0.4.3'
---
+++ build.rb
@@ -0,0 +1,19 @@
+require 'goodreads'
+
+$client = Goodreads.new(api_key: "API_KEY_HERE")
+$user_id = '16597993'
+
+
+def print_shelf(shelf_name)
+  shelf = $client.shelf($user_id, shelf_name)
+  shelf.books[1..10].each do |review|
+    link = "<a href='#{review.book.url}'>#{review.book.title}</a>"
+    puts "#{review.date_updated[4..9]} #{review.date_updated[26..30]} - #{link}"
+  end
+end
+
+puts "Currently Reading"
+print_shelf('currently-reading')
+
+puts "Last read"
+print_shelf('read')
