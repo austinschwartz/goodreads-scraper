8c62e520be744451e6ea974486bda8e89bed3224
nonis3 <nonis@purdue.edu>
2017-04-28 00:51:47 +0000

    Spacing

--- build.rb
+++ build.rb
@@ -3,7 +3,6 @@ require 'goodreads'
 $client = Goodreads.new(api_key: "API_KEY_HERE")
 $user_id = '16597993'
 
-
 def print_shelf(shelf_name)
   shelf = $client.shelf($user_id, shelf_name)
   shelf.books[1..10].each do |review|
