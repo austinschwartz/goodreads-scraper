10523b605d4542ad048870a94c58ad86c8f0569e
nonis3 <nonis@purdue.edu>
2017-06-09 01:14:31 -0400

    Fix all the unnecessary ifs later, but now it makes a list for first section, table for second

--- build.rb
+++ build.rb
@@ -29,15 +29,18 @@ end
 
 def print_shelf(shelf, include_date=FALSE, count=5, favorites_set=Set.new)
   shelf.first(count).each do |review|
-    print "<li>"
+    print "<tr>" if include_date
     if include_date then
       new_date = DateTime.strptime(review.read_at, '%a %b %d %H:%M:%S %z %Y')
-      print "<code>#{new_date.strftime('%m/%d/%y')}</code>"
+      print "<td><code style=\"vertical-align:bottom\">#{new_date.strftime('%m/%d/%y')}</code></td>"
     end
+    print "<td>" if include_date
+    puts "<li>" if !include_date
     print "<i><b>" if favorites_set.include? review.id
     print "<a href=\"#{review.book.link}\">#{review.book.title}</a>"
     print "</b></i>" if favorites_set.include? review.id
-    print "</li>"
+    print "</li>" if !include_date
+    print "</td></tr>" if include_date
     puts ""
   end
 end
@@ -53,9 +56,10 @@ print_shelf(currently_reading, include_date=FALSE, count=5, favorites_set=favori
 puts "</ul>"
 
 puts "<h2>Last read</h2>"
-puts "<ul style='list-style:none'>"
+puts "<table class='books'>"
 print_shelf(read, include_date=TRUE, count=20, favorites_set=favorites_set)
-puts "</ul>"
+puts "</table>"
+puts "</br>"
 puts "This list automatically generated from my <a href=\"https://www.goodreads.com/user/show/16597993-austin\">my goodreads account</a> by <a href=\"https://github.com/nonis3/goodreads-scraper\">this script</a>. Books in italics are tagged on goodreads as those I particularly liked."
 puts "</div>"
 
