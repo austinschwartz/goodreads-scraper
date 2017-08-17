bb7e5a09bc80304a34475a2a71eb95f4dc07ef09
nonis3 <nonis@purdue.edu>
2017-08-17 18:44:26 -0400

    Removing deploy script :)

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ruby build.rb > books.html
-cp books.html /home/nonis/Projects/blog/partials/books.html
-cd ~/Projects/blog
-sudo /home/nonis/Projects/blog/compile.sh
-sudo /home/nonis/Projects/blog/deploy.sh
